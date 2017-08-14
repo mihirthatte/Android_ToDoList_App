da8ef45496b6e675ec468d4f304f1bd7a11fd112
Mihir Thatte <mihir.m.thatte@gmail.com>
2017-08-14 16:50:37 -0400

    Button function commented out

--- SimpleTodo/app/src/main/java/com/example/mthat/simpletodo/EditItemActivity.java
+++ SimpleTodo/app/src/main/java/com/example/mthat/simpletodo/EditItemActivity.java
@@ -1,22 +1,12 @@
 package com.example.mthat.simpletodo;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-        import android.app.Activity;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-
-import static android.R.attr.button;
-import static com.example.mthat.simpletodo.R.id.etNewItem;
-import static com.example.mthat.simpletodo.R.id.lvItems;
-import static com.example.mthat.simpletodo.R.id.savebtn;
 
 public class EditItemActivity extends AppCompatActivity {
     int index;
@@ -42,10 +32,15 @@ public class EditItemActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-
     }
 
-
+    /*public void saveItem(View v) {
+        EditText editedtext = (EditText) findViewById(R.id.editItem);
+        Intent response = new Intent();
+        response.putExtra("editedValue",editedtext.getText().toString());
+        response.putExtra("index", index);
+        setResult(Activity.RESULT_OK, response);
+        finish();
+    }*/
 
 }
